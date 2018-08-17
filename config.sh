b13ca27ebfb076117bd8044b7df9d1cf014215d8
Sean Gillies <sean.gillies@gmail.com>
2018-08-17 11:16:29 +0200

    Move vars to env_vars.sh

--- config.sh
+++ config.sh
@@ -2,26 +2,6 @@
 #
 # Test for OSX with [ -n "$IS_OSX" ].
 
-ARCHIVE_SDIR=rasterio
-
-# Package versions for fresh source builds.
-# Copied from Pillow wheels project.
-LIBPNG_VERSION=1.6.35
-ZLIB_VERSION=1.2.11
-JPEG_VERSION=9c
-# OPENJPEG_VERSION=2.1
-TIFF_VERSION=4.0.9
-LIBWEBP_VERSION=1.0.0
-
-# Specific to Rasterio
-OPENJPEG_VERSION=2.3.0
-GEOS_VERSION=3.6.2
-JSONC_VERSION=0.12
-PROJ_VERSION=4.9.3
-SQLITE_VERSION=sqlite-autoconf-3240000
-GDAL_VERSION=2.2.4
-CURL_VERSION=7.61.0
-
 
 function build_geos {
     build_simple geos $GEOS_VERSION https://download.osgeo.org/geos tar.bz2
