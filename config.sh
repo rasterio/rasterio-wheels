75ababf94d532fe6461b9cedaae6c637798790a6
Sean Gillies <sean.gillies@gmail.com>
2018-09-06 17:12:53 -0600

    Update readme (#1)
    * Update readme
    
    * Fix bash syntax error
    
    * Add missing archive extensions
    
    * Suppress warning
    
    * remove whitespace
    
    * Add patch file
    
    * Fix relative path to patch file
    
    * Update curl version
    
    * CURL_VERSION not LIBCURL_VERSION
    
    * -I not -L in multibuild
    
    * Get PR #193
    
    * Use include-fix branch
    
    * export macosx deployment target
    
    * macosx deployment target in travis env
    
    * Move vars to env_vars.sh
    
    * Try linux instead of os x
    
    * Reformat
    
    * Custom sqlite recipe
    
    * Build cmake 3.12 on linux
    
    * Silence cmake build
    
    * Add GDAL_CONFIG
    
    * Customize openjpeg build
    
    * Print formats
    
    * Suppress some output
    
    * Call gdal-config only if build succeeds
    
    * De-parallelize build
    
    * GDAL 2.3.1
    
    * Build newer expat on linux
    
    * export EXPAT_PREFIX
    
    * set EXPAT_VERSION
    
    * Fix build and test dependencies
    
    * Remove pytest installation from run_tests
    
    * Run tests out of repo
    
    * Add more test dependencies
    
    * Add vars for click and gdal-bin apt package
    
    * Install gdal-bin later
    
    * Remove some global env
    
    * apt-get update
    
    * Add package data to wheels
    
    * More debugging, unsetting of PROJ_LIB in tests
    
    * /io/wheelhouse
    
    * suppress gdal output
    
    * start_spinner
    
    * remove spinner
    
    * unzip -l
    
    * export env vars
    
    * configure curl certs
    
    * add ca-certificates
    
    * openssl 1.0.2o + cainfo
    
    * ca-certificates plural
    
    * customize tiff build
    
    * Remove bucket name
    
    * Add macosx matrix element
    
    * Reset artifacts path
    
    * Fix up yaml
    
    * Customize hdf5 builder
    
    * suppress hdf5 build output
    
    * Set stdout's file status to 0
    
    See https://github.com/travis-ci/travis-ci/issues/8920#issuecomment-352767521
    
    * no filter
    
    * Revert to curl 7.51
    
    * Build curl -Wno-error
    
    * Back to curl 7.60
    
    * Fix configuration of libjson-c
    
    * Speed up mac builds
    
    * Add caching and a dependency cache warm up step
    
    * Download a pre-built GEOS lib
    
    * Return GDAL to library builds
    
    * sudo cp
    
    * Fix filename
    
    * Also bundle hdf5 and netcdf
    
    * nc-config
    
    * make -j4 (gdal)
    
    * Bundle geos, hdf, netcdf together
    
    build prefix is now /gdal
    
    * Revert to /usr/local for library builds
    
    * unzip not untar
    
    * copy file to /tmp
    
    * Prevent /opt/local python from superseding our venv
    
    * Remove webp, bsb, pcraster, pcidsk, test for formats later
    
    * remove gdal-config call
    
    * Change locale from C to en_US, cache pip
    
    * C locale on linux, suppress gdal build output
    
    * confirm unzip overwrite
    
    * exclude test_open_https_vsicurl
    
    until we can properly skip it.
    
    * Set DEPS_PREFIX
    
    * Turn on GDAL output
    
    * de-quiet
    
    * Disable sfcgal
    
    * Moar output
    
    * export deps prefix
    
    * Turn up unzip verbosity
    
    * Don't use nc-config
    
    * Fill out matrix
    
    * Remove 32-bit linux from matrix, add pythons 2.7 and 3.4
    
    * Comment out linux + python 3.4

--- config.sh
+++ config.sh
@@ -2,34 +2,14 @@
 #
 # Test for OSX with [ -n "$IS_OSX" ].
 
-ARCHIVE_SDIR=rasterio
-MACOSX_DEPLOYMENT_TARGET=10.9
-
-# Package versions for fresh source builds.
-# Copied from Pillow wheels project.
-LIBPNG_VERSION=1.6.35
-ZLIB_VERSION=1.2.11
-JPEG_VERSION=9c
-# OPENJPEG_VERSION=2.1
-TIFF_VERSION=4.0.9
-LIBWEBP_VERSION=1.0.0
-
-# Specific to Rasterio
-OPENJPEG_VERSION=2.3.0
-GEOS_VERSION=3.6.2
-JSONC_VERSION=0.12
-PROJ_VERSION=4.9.3
-SQLITE_VERSION=sqlite-autoconf-3240000
-GDAL_VERSION=2.2.4
-
 
 function build_geos {
-    build_simple geos $GEOS_VERSION https://download.osgeo.org/geos
+    build_simple geos $GEOS_VERSION https://download.osgeo.org/geos tar.bz2
 }
 
 
 function build_jsonc {
-    build_simple json-c $JSONC_VERSION https://s3.amazonaws.com/json-c_releases/releases
+    build_simple json-c $JSONC_VERSION https://s3.amazonaws.com/json-c_releases/releases tar.gz
 }
 
 
@@ -37,7 +17,7 @@ function build_proj {
     if [ -e proj-stamp ]; then return; fi
     fetch_unpack http://download.osgeo.org/proj/proj-${PROJ_VERSION}.tar.gz
     (cd proj-${PROJ_VERSION} \
-        && patch -u -p1 < patches/bd6cf7d527ec88fdd6cc3f078387683d683d0445.diff
+        && patch -u -p1 < ../patches/bd6cf7d527ec88fdd6cc3f078387683d683d0445.diff \
         && ./configure --prefix=$BUILD_PREFIX \
         && make -j4 \
         && make install)
@@ -46,12 +26,147 @@ function build_proj {
 
 
 function build_sqlite {
-    build_simple sqlite $SQLITE_VERSION https://www.sqlite.org/2018
+    if [ -e sqlite-stamp ]; then return; fi
+    fetch_unpack https://www.sqlite.org/2018/sqlite-autoconf-${SQLITE_VERSION}.tar.gz
+    (cd sqlite-autoconf-${SQLITE_VERSION} \
+        && ./configure --prefix=$BUILD_PREFIX \
+        && make -j4 \
+        && make install)
+    touch sqlite-stamp
+}
+
+
+function build_expat {
+    if [ -e expat-stamp ]; then return; fi
+    if [ -n "$IS_OSX" ]; then
+        :
+    else
+        fetch_unpack https://github.com/libexpat/libexpat/releases/download/R_2_2_6/expat-${EXPAT_VERSION}.tar.bz2
+        (cd expat-${EXPAT_VERSION} \
+            && ./configure --prefix=$BUILD_PREFIX \
+            && make -j4 \
+            && make install)
+    fi
+    touch expat-stamp
+}
+
+
+function get_cmake {
+    local cmake=cmake
+    if [ -n "$IS_OSX" ]; then
+        brew install cmake > /dev/null
+    else
+        fetch_unpack https://www.cmake.org/files/v3.12/cmake-3.12.1.tar.gz > /dev/null
+        (cd cmake-3.12.1 \
+            && ./bootstrap --prefix=$BUILD_PREFIX > /dev/null \
+            && make -j4 > /dev/null \
+            && make install > /dev/null)
+        cmake=/usr/local/bin/cmake
+    fi
+    echo $cmake
+}
+
+
+function build_openjpeg {
+    if [ -e openjpeg-stamp ]; then return; fi
+    build_zlib
+    build_libpng
+    build_tiff
+    build_lcms2
+    local cmake=$(get_cmake)
+    local archive_prefix="v"
+    if [ $(lex_ver $OPENJPEG_VERSION) -lt $(lex_ver 2.1.1) ]; then
+        archive_prefix="version."
+    fi
+    local out_dir=$(fetch_unpack https://github.com/uclouvain/openjpeg/archive/${archive_prefix}${OPENJPEG_VERSION}.tar.gz)
+    (cd $out_dir \
+        && $cmake -DBUILD_THIRDPARTY:BOOL=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX . \
+        && make -j4 \
+        && make install)
+    touch openjpeg-stamp
+}
+
+
+function build_tiff {
+    build_zlib
+    build_jpeg
+    build_simple tiff $TIFF_VERSION https://download.osgeo.org/libtiff
+}
+
+
+function build_hdf5 {
+    if [ -e hdf5-stamp ]; then return; fi
+    build_zlib
+    # libaec is a drop-in replacement for szip
+    build_libaec
+    local hdf5_url=https://support.hdfgroup.org/ftp/HDF5/releases
+    local short=$(echo $HDF5_VERSION | awk -F "." '{printf "%d.%d", $1, $2}')
+    fetch_unpack $hdf5_url/hdf5-$short/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz
+    (cd hdf5-$HDF5_VERSION \
+        && ./configure --enable-shared --enable-build-mode=production --with-szlib=$BUILD_PREFIX --prefix=$BUILD_PREFIX \
+        && make -j4 \
+        && make install)
+    touch hdf5-stamp
+}
+
+
+function build_curl {
+    if [ -e curl-stamp ]; then return; fi
+    local flags="--prefix=$BUILD_PREFIX"
+    if [ -n "$IS_OSX" ]; then
+        flags="$flags --with-darwinssl"
+    else  # manylinux
+        flags="$flags --with-ssl"
+        build_openssl
+    fi
+    fetch_unpack https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz
+    (cd curl-${CURL_VERSION} \
+        && if [ -z "$IS_OSX" ]; then \
+        LIBS=-ldl ./configure $flags; else \
+        ./configure $flags; fi\
+        && make -j4 CFLAGS=-Wno-error \
+        && make install)
+    touch curl-stamp
+}
+
+
+function build_bundled_deps {
+    if [ -n "$IS_OSX" ]; then
+        curl -fsSL -o /tmp/deps.zip https://github.com/sgillies/rasterio-wheels/files/2350174/gdal-deps.zip
+        (cd / && sudo unzip -o /tmp/deps.zip)
+        /gdal/bin/nc-config --libs
+        touch geos-stamp && touch hdf5-stamp && touch netcdf-stamp
+    else
+        start_spinner
+        suppress build_geos
+        suppress build_hdf5
+        suppress build_netcdf
+        stop_spinner
+    fi
 }
 
 
 function build_gdal {
     if [ -e gdal-stamp ]; then return; fi
+    build_jpeg
+    build_tiff
+    build_libpng
+    build_openjpeg
+    build_jsonc
+    build_proj
+    build_sqlite
+    build_curl
+    build_expat
+    build_bundled_deps
+
+    if [ -n "$IS_OSX" ]; then
+        EXPAT_PREFIX=/usr
+        DEPS_PREFIX=/gdal
+    else
+        EXPAT_PREFIX=$BUILD_PREFIX
+        DEPS_PREFIX=$BUILD_PREFIX
+    fi
+
     fetch_unpack http://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION}.tar.gz
     (cd gdal-${GDAL_VERSION} \
         && ./configure \
@@ -63,29 +178,29 @@ function build_gdal {
             --without-libgrass \
             --without-jpeg12 \
             --without-jasper \
+            --without-bsb \
             --without-python \
-            --with-netcdf=${BUILD_PREFIX}/netcdf \
-            --with-openjpeg=${BUILD_PREFIX}/openjpeg \
+            --with-freexl=no \
+            --with-netcdf=${DEPS_PREFIX} \
+            --with-openjpeg=${BUILD_PREFIX} \
             --with-libtiff=${BUILD_PREFIX}/tiff \
-            --with-webp=${BUILD_PREFIX}/webp \
             --with-jpeg \
             --with-gif \
             --with-png \
             --with-geotiff=internal \
             --with-sqlite3=${BUILD_PREFIX}/sqlite \
-            --with-pcraster=internal \
-            --with-pcraster=internal \
-            --with-pcidsk=internal \
-            --with-bsb \
+            --with-pcraster=no \
+            --with-pcidsk=no \
+            --with-sfcgal=no \
             --with-grib \
             --with-pam \
-            --with-geos=${BUILD_PREFIX}/bin/geos-config \
-            --with-static-proj4=${BUILD_PREFIX}/proj4 \
-            --with-expat=/usr \
-            --with-libjson-c=${BUILD_PREFIX}/json-c \
+            --with-geos=${DEPS_PREFIX}/bin/geos-config \
+            --with-proj=${BUILD_PREFIX}/proj4 \
+            --with-expat=${EXPAT_PREFIX} \
+            --with-libjson-c=${BUILD_PREFIX} \
             --with-libiconv-prefix=/usr \
             --with-libz=/usr \
-            --with-curl=curl-config \
+            --with-curl=${BUILD_PREFIX}/bin/curl-config \
         && make -j4 \
         && make install)
     touch gdal-stamp
@@ -95,31 +210,47 @@ function build_gdal {
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    if [ -n "$IS_OSX" ]; then
-        # Update to latest zlib for OSX build
-        build_new_zlib
-    fi
-    build_jpeg
-    build_tiff
-    build_libpng
-    build_openjpeg
+    #if [ -n "$IS_OSX" ]; then
+    #    # Update to latest zlib for OSX build
+    #    build_new_zlib
+    #fi
 
-#    if [ -n "$IS_OSX" ]; then
-#        # Fix openjpeg library install id
-#        # https://code.google.com/p/openjpeg/issues/detail?id=367
-#        install_name_tool -id $BUILD_PREFIX/lib/libopenjp2.7.dylib $BUILD_PREFIX/lib/libopenjp2.2.1.0.dylib
-#    fi
+    build_bundled_deps
 
-    build_libwebp
-    build_geos
-    build_jsonc
-    build_proj
-    build_netcdf
-    build_sqlite
+    start_spinner
+    suppress build_jpeg
+    suppress build_tiff
+    suppress build_libpng
+    suppress build_openjpeg
+    suppress build_jsonc
+    suppress build_proj
+    suppress build_sqlite
+    suppress build_curl
+    suppress build_expat
+    stop_spinner
+
+    build_gdal
 }
 
 
 function run_tests {
-    # Runs tests on installed distribution from an empty directory
-    cd ../rasterio && python -m pytest -vv
+    unset PROJ_LIB
+    if [ -n "$IS_OSX" ]; then
+        export PATH=$PATH:${BUILD_PREFIX}/bin
+        export LC_ALL=en_US.UTF-8
+        export LANG=en_US.UTF-8
+    else
+        export LC_ALL=C.UTF-8
+        export LANG=C.UTF-8
+        export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+        sudo apt-get update
+        sudo apt-get install -y gdal-bin ca-certificates
+    fi
+    cd ../rasterio
+    mkdir -p /tmp/rasterio
+    cp -R tests /tmp/rasterio
+    cd /tmp/rasterio
+    python -m pytest -vv -k "not test_open_https_vsicurl" tests
+    rio --version
+    rio env --formats
 }
