e0a8028d979f5491a8229210adbb9276d20285f4
Sean C. Gillies <sean@mapbox.com>
2018-09-07 14:45:38 -0600

    Return vsicurl test

--- config.sh
+++ config.sh
@@ -252,7 +252,7 @@ function run_tests {
     mkdir -p /tmp/rasterio
     cp -R tests /tmp/rasterio
     cd /tmp/rasterio
-    python -m pytest -vv -k "not test_open_https_vsicurl" tests
+    python -m pytest -vv tests
     rio --version
     rio env --formats
 }
