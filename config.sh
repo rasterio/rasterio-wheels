e04421f4ffe7f2b9edca36766e597036ac40a8c5
Sean C. Gillies <sean@mapbox.com>
2018-10-16 11:06:54 -0600

    Add and use new GDAL patch
    Resolves #7

--- config.sh
+++ config.sh
@@ -171,6 +171,7 @@ function build_gdal {
     fetch_unpack http://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION}.tar.gz
     (cd gdal-${GDAL_VERSION} \
         && patch -u < ../patches/pkg-config.patch \
+        && patch -p1 -u < ../patches/340ad0d703534a256ec3de94176c95b0cf20cbd4.diff \
         && ./configure \
             --prefix=$BUILD_PREFIX \
             --with-threads \
