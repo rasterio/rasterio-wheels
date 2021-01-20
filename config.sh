7d77545fd218062f2e0644f09d52757044dcd85f
Sean Gillies <sean@mapbox.com>
2021-01-20 10:59:47 -0700

    Build 4491f2d

--- config.sh
+++ config.sh
@@ -197,7 +197,7 @@ function build_gdal {
 	        --with-webp=${BUILD_PREFIX} \
             --disable-debug \
             --disable-static \
-	    --disable-driver-elastic \
+	        --disable-driver-elastic \
             --prefix=$BUILD_PREFIX \
             --with-curl=curl-config \
             --with-expat=${EXPAT_PREFIX} \
