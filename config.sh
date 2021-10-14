f40f9e390ef46123532d72a5bbc3b84fe83446f2
Sean Gillies <sean@mapbox.com>
2021-10-14 16:08:17 -0600

    Test build of 1.3a1 with a patch for GDAL #4646

--- config.sh
+++ config.sh
@@ -207,6 +207,7 @@ function build_gdal {
 
     fetch_unpack http://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION}.tar.gz
     (cd gdal-${GDAL_VERSION} \
+        && (patch -u -p2 --force < ../patches/4646.diff || true) \
         && ./configure \
 	        --with-crypto=yes \
 	        --with-hide-internal-symbols \
