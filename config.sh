bdd9582fd26afeaee068c1f0d183eea51c48f53d
Sean Gillies <sean.gillies@gmail.com>
2023-01-30 18:03:48 -0700

    gdal-config broken?

--- config.sh
+++ config.sh
@@ -397,7 +397,8 @@ function build_wheel_cmd {
     if [ -n "$BUILD_DEPENDS" ]; then
         pip install $(pip_opts) $BUILD_DEPENDS
     fi
-    (cd $repo_dir && GDAL_CONFIG=$BUILD_PREFIX/bin/gdal-config PIP_NO_BUILD_ISOLATION=0 PIP_USE_PEP517=0 $cmd $wheelhouse)
+    $BUILD_PREFIX/bin/gdal-config
+    (cd $repo_dir && PIP_NO_BUILD_ISOLATION=0 PIP_USE_PEP517=0 $cmd $wheelhouse)
     if [ -n "$IS_OSX" ]; then
 	:
     else  # manylinux
